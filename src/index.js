b417ef5d7099457b32990b57ff0663245cc0bea1
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2023-07-28 17:23:23 +0200

    moved get Forecast Function and the forecast api url

--- src/index.js
+++ src/index.js
@@ -89,8 +89,8 @@ newTempF.addEventListener("click", changeFarenheitTemp);
 let celciusTemperature = null;
 
 function getForecast(coordinates) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -148,6 +148,8 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
+getForecast(response.data.coord);
 
 //function getCurrentPosition() {
 //navigator.geolocation.getCurrentPosition(showPosition);
